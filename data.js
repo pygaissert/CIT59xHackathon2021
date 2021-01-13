7deb804b48cc82bba87a6a2c7b87f16b94fa99c7
Philipp Gaissert <pgaissert@gmail.com>
2021-01-13 17:48:23 -0500

    Created data.js module, created new functions in views.js

---
+++ data.js
@@ -0,0 +1,85 @@
+// data.js
+// This is a module for interacting with the MongoDB database
+
+// FOR INITIALIZING MONGODB CLIENTS
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_CONNECTION;
+
+const userExists = async function(userId) {
+  // Create new MongoDB client
+  let client = new MongoClient(uri, { useUnifiedTopology: true });
+  // Connect client to MongoDB cluster
+  await client.connect();
+  collection = await client.db("app-data").collection("users");
+  exists = await collection.findOne( {slack_id: userId});
+  if (exists) {
+    return true;
+  } else {
+    return false;
+  }
+  client.close();
+}
+
+const addUser = async function(userName, userId) {
+  // Create new MongoDB client
+  let client = new MongoClient(uri, { useUnifiedTopology: true });
+  // Connect client to MongoDB cluster
+  await client.connect();
+  // Get "users" collection from "test_slack" database
+  collection = await client.db("app-data").collection("users");
+  user = {
+    name: userName,
+    slack_id: userId
+  }
+  await collection.insertOne(user, {w: 1}, function(err, doc) {
+    if (err) {
+      console.log(err);
+      process.exit(0);
+    }
+    let saved = doc.ops[0];
+    console.log(`${saved._id}: ${saved.name} (${saved.slack_id})`);
+    // Disconnect client from MongoDB cluster
+    client.close();
+  });
+}
+
+const listSkills = async function() {
+  // Create new MongoDB client
+  let client = new MongoClient(uri, { useUnifiedTopology: true });
+  // Connect client to MongoDB cluster
+  await client.connect();
+  // Get "skills" collection from "test_slack" database
+  collection = await client.db("slack-app").collection("topics");
+  // Get array of skill groups from "skills" collection
+  groups = await collection.distinct("group");
+  // Get array of skills from "skills" collection
+  //skills = await collection.find({}).sort({group: 1, name: 1}).toArray();
+  // Initialize an empty options array for the dropdown menu
+  let option_groups = [];
+  // Add each skill's name to the options array
+  for (group of groups) {
+    let options = [];
+    let skills = await collection.find({group: group}).sort({name: 1}).toArray();
+    for (skill of skills) {
+      options.push({
+        text: {
+          type: 'plain_text',
+          text: skill.name
+        }
+      });
+    }
+    option_groups.push({
+      label: {
+        type: 'plain_text',
+        text: group
+      },
+      options: options
+    });
+  }
+  return option_groups;
+}
+
+module.exports = {
+  addUser: addUser,
+  userExists: userExists
+}
--- views.js
+++ views.js
@@ -1,47 +1,126 @@
-//
+// views.js
+// This is a module for creating modal views
+
+const newUserGreeting = function (user) {
+  return {
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${user}>!\nWould you like to join?`
+        },
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "YES"
+            },
+            "style": "primary",
+            "action_id": "button_yes"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "NO"
+            },
+            "action_id": "button_no"
+          }
+        ]
+      }
+    ],
+    text: `Hey there <@${user}>!`
+  }
+};
+
+const existingUserGreeting = function (user) {
+  return {
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${user}>!\nHow can I help you?`
+        },
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Edit Profile"
+            },
+            "style": "primary",
+            "action_id": "button_edit"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Ask a Question"
+            },
+            "action_id": "button_ask"
+          }
+        ]
+      }
+    ],
+    text: `Hey there <@${user}>!`
+  }
+};
+
+const test = {
+  type: 'modal',
+  callback_id: 'view_1',
+  title: {
+    type: 'plain_text',
+    text: 'Modal title'
+  },
+  blocks: [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'Welcome to a modal with _blocks_'
+      },
+      accessory: {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Click Me!'
+        },
+        action_id: 'button_abc'
+      }
+    },
+    {
+      type: 'input',
+      block_id: 'input_c',
+      label: {
+        type: 'plain_text',
+        text: 'What are your hopes and dreams?'
+      },
+      element: {
+        type: 'plain_text_input',
+        action_id: 'dreamy_input',
+        multiline: true
+      }
+    }
+  ],
+  submit: {
+    type:'plain_text',
+    text: 'Submit'
+  }
+};
 
 module.exports = {
   // Template modal from Slack's website
-  test: {
-    type: 'modal',
-    callback_id: 'view_1',
-    title: {
-      type: 'plain_text',
-      text: 'Modal title'
-    },
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: 'Welcome to a modal with _blocks_'
-        },
-        accessory: {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Click Me!'
-          },
-          action_id: 'button_abc'
-        }
-      },
-      {
-        type: 'input',
-        block_id: 'input_c',
-        label: {
-          type: 'plain_text',
-          text: 'What are your hopes and dreams?'
-        },
-        element: {
-          type: 'plain_text_input',
-          action_id: 'dreamy_input',
-          multiline: true
-        }
-      }
-    ],
-    submit: {
-      type:'plain_text',
-      text: 'Submit'
-    }
-  }
+  test: test,
+  newUserGreeting: newUserGreeting,
+  existingUserGreeting: existingUserGreeting
 }
